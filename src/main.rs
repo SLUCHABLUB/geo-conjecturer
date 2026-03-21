9b19a7a37e6f73b9f62f570bee5099e871a8da5c
Nelli Skogman <nelli.skogman@gmail.com>
2026-03-21 21:03:06 +0100

    Add root route.

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,33 @@
-fn main() {}
+use axum::Router;
+use axum::response::Html;
+use axum::routing::get;
+use maud::DOCTYPE;
+use maud::html;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:1337").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> Html<String> {
+    Html(
+        html! {
+            (DOCTYPE)
+            html {
+                head {
+                    title {
+                        "GeoConjecturer"
+                    }
+                }
+                body {
+                    "welcome"
+                }
+            }
+        }
+        .into_string(),
+    )
+}
