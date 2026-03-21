ad9a1dbf7997423966247c92e0dc3167c374c4d6
Nelli Skogman <nelli.skogman@gmail.com>
2026-03-21 23:52:20 +0100

    Add argument parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,46 @@
 use axum::Router;
 use axum::response::Html;
 use axum::routing::get;
+use clap::Parser;
+use dotenv::dotenv;
 use maud::DOCTYPE;
 use maud::html;
+use std::net::Ipv4Addr;
+use tokio::net::TcpListener;
+
+#[derive(Parser)]
+struct Arguments {
+    #[arg(short = 'N', long, env = "LATITUDE")]
+    latitude: f32,
+    #[arg(short = 'E', long, env = "LONGITUDE")]
+    longitude: f32,
+    #[arg(short = 'z', long, env = "ZOOM")]
+    zoom: u8,
+
+    #[arg(short, long, env = "PORT", default_value = "1337")]
+    port: u16,
+}
+
+fn make_static<T>(value: T) -> &'static T {
+    Box::leak(Box::new(value))
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root));
+    let _ = dotenv();
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:1337").await.unwrap();
+    let arguments = make_static(Arguments::parse());
+
+    let app = Router::new().route("/", get(|| root(arguments)));
+
+    let listener = TcpListener::bind((Ipv4Addr::UNSPECIFIED, arguments.port))
+        .await
+        .unwrap();
 
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn root() -> Html<String> {
+async fn root(arguments: &Arguments) -> Html<String> {
     Html(
         html! {
             (DOCTYPE)
@@ -34,6 +61,8 @@ async fn root() -> Html<String> {
                     div
                         id="map"
                         style="height: 100vh"
+                        data-center={"[" (arguments.latitude) "," (arguments.longitude) "]"}
+                        data-zoom=(arguments.zoom)
                     {
                         div data-tile="OpenStreetMap" data-default-tile {}
                     }
